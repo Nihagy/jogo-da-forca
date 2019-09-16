6d447fe7f90938c5411f992a297ca9289802513b
mouraCorazim <lucasmouracorazim1@gmail.com>
2019-09-16 14:33:22 -0300

    Travar a tecla já pressionada

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,66 @@
-let palavras = [
+const divsPalavra = document.getElementsByClassName('palavra')
+const indice = Math.floor(Math.random()*3)
+const letrasParaTeclado = "abcdefghijklmnopqrstuvwxyz"
+const palavras = [
     {'palavra': 'banana' , 'dica': 'fruta' },
+    {'palavra': 'uva' , 'dica': 'fruta' },
+    {'palavra': 'manga' , 'dica': 'fruta' },
+    {'palavra': 'morango' , 'dica': 'fruta' },
 ]
-let teclado = document.getElementsByClassName('teclas')
-let divsPalavra = document.getElementsByClassName('palavra')
-let letras = "abcdefghijklmnopqrstuvwxyz"
-let palavra = palavras[0].palavra
+const dica = palavras[indice].dica
+const palavra = palavras[indice].palavra
 const palavraEmArray = palavra.split('')
+const teclado = document.getElementsByClassName('teclas')
 
-const compararLetras = (letra) => {
-    if(palavraEmArray.includes(letra)){
-        console.log("Letra que pertence:", letra)
-    }
-    else{
-        console.log("Letra que não pertence:", letra)
-    }
-    
-}
-
-const pegarValor = (event) => {
-    const letra = event.target.innerHTML
-    compararLetras(letra)
-    console.log(letra)
-}
-
-const atribuirValores = () => {
-
-    for(let i = 0; i < teclado.length; i++){
-        teclado[i].innerHTML = letras[i]
-        teclado[i].onclick = pegarValor
-    }
-}
 
 const atribuirPalavra = () => {
     for(let i = 0; i < palavraEmArray.length; i++){
         divsPalavra[i].innerHTML = palavraEmArray[i]
     }
+    escreverDica(dica)
+}
+
+const atribuirValores = () => {
+
+    for(let i = 0; i < teclado.length; i++){
+        teclado[i].innerHTML = letrasParaTeclado[i]
+        teclado[i].onclick = pegarValor
+    }
+    atribuirPalavra()
+}
+
+const compararLetras = (letra, tecla) => {
+    tecla.onclick = null
+
+    if(palavraEmArray.includes(letra)){
+        tecla.className = 'correta'
+        mostrarPalavra(letra)
+    }
+    else{
+        tecla.className = 'incorreta'
+    }
+}
+
+const escreverDica = (dica) => {
+}
+
+const mostrarPalavra = (letra) => {
+
+    for(let i = 0; i < divsPalavra.length; i++){
+        if(divsPalavra[i].innerHTML === letra){
+            divsPalavra[i].style.display = 'block'
+        }
+    }
+}
+
+
+const pegarValor = (event) => {
+    const letra = event.target.innerHTML
+    const tecla = event.target
+    compararLetras(letra, tecla)
 }
 
 atribuirValores()
-atribuirPalavra()
 
 
 
