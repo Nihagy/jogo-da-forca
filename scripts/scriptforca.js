9b75555a4673f29e6e8b75e25cdd92f5df71a36a
mouraCorazim <lucasmouracorazim1@gmai.com>
2019-09-20 16:38:04 -0300

    Organizar script e criar classe .span-inativo

--- scripts/scriptforca.js
+++ scripts/scriptforca.js
@@ -1,54 +1,86 @@
 const divsPalavra = document.querySelectorAll('section div')
-const spanPalavra = document.querySelectorAll('section span')
-const indice = Math.floor(Math.random()*4)
-const letrasParaTeclado = "abcdefghijklmnopqrstuvwxyz"
-const palavras = [
-    {'palavra': 'banana' , 'dica': 'Fruta' },
-    {'palavra': 'uva' , 'dica': 'Fruta' },
-    {'palavra': 'manga' , 'dica': 'Fruta' },
-    {'palavra': 'morango' , 'dica': 'Fruta' },
-]
-const dica = palavras[indice].dica
-const palavra = palavras[indice].palavra
-const teclado = document.getElementsByClassName('teclas')
-const paragrafoParaDica = document.querySelector('header p')
 
+const escolherPalavra = () => {
 
-const atribuirPalavra = () => {
-    for(let i = 0; i < palavra.length; i++){
-        divsPalavra[i].innerHTML = palavra[i]
-        divsPalavra[i].className = 'inativo' 
-        divsPalavra[i].style.gridArea = "d"+ i
-        spanPalavra[i].style.gridArea = "t"+ i
-    }
+    const indice = Math.floor(Math.random()*4)
+
+    const palavras = [
+        {'palavra': 'banana' , 'dica': 'Fruta' },
+        {'palavra': 'uva' , 'dica': 'Fruta' },
+        {'palavra': 'manga' , 'dica': 'Fruta' },
+        {'palavra': 'morango' , 'dica': 'Fruta' },
+    ]
+
+    const dica = palavras[indice].dica
+
+    const palavra = palavras[indice].palavra
+
+    atribuirCamposTracos(palavra)
+
+    atribuirCamposPalavra(palavra)
+    
     escreverDica(dica)
+
+    return palavra
 }
 
-const atribuirTracos = () => {
+
+const atribuirCamposTracos = (palavra) => {
+    
+    const spanPalavra = document.querySelectorAll('section span')
+    
+    for(let i = 0; i < palavra.length; i++){
+        spanPalavra[i].style.gridArea = "t"+ i
+    }
+    
     let l = divsPalavra.length - palavra.length
+    
     for(let i = divsPalavra.length - 1; l > 0 ; i-- ){
-        spanPalavra[i].className = 'inativo'
+        spanPalavra[i].className = 'span-inativo'
         l--
     }
 }
 
-const atribuirValores = () => {
+const atribuirCamposPalavra = (palavra) => {
+    
+    for(let i = 0; i < palavra.length; i++){
+        divsPalavra[i].innerHTML = palavra[i]
+        divsPalavra[i].className = 'inativo' 
+        divsPalavra[i].style.gridArea = "d"+ i
+    }
+}
+
+const escreverDica = (dica) => {
+    
+    const paragrafoParaDica = document.querySelector('header p')
+    
+    paragrafoParaDica.innerHTML = dica
+}
+
+const atribuirValoresDoTeclado = () => {
+    
+    const letrasParaTeclado = "abcdefghijklmnopqrstuvwxyz"
+    
+    const teclado = document.getElementsByClassName('teclas')
     
     for(let i = 0; i < teclado.length; i++){
         teclado[i].innerHTML = letrasParaTeclado[i]
-        teclado[i].onclick = pegarValor
+        teclado[i].onclick = pegarValorTeclado
     }
-    atribuirPalavra()
+    
 }
 
+const palavra = escolherPalavra()
+
 const compararLetras = (letra, tecla) => {
+    
     const palavraEmArray = palavra.split('')
+    
     tecla.onclick = null
     
     if(palavraEmArray.includes(letra)){
         tecla.className = 'correta'
-        mostrarPalavra(letra)
-        verificarVitoria()
+        mostrarLetras(letra)
     }
     else{
         tecla.className = 'incorreta'
@@ -56,23 +88,22 @@ const compararLetras = (letra, tecla) => {
     }
 }
 
-const escreverDica = (dica) => {
-    paragrafoParaDica.innerHTML = dica
-}
 
-const mostrarPalavra = (letra) => {
+const mostrarLetras = (letra) => {
 
     for(let i = 0; i < divsPalavra.length; i++){
         if(divsPalavra[i].innerHTML === letra){
             divsPalavra[i].className = "palavra"
         }
     }
+    verificarVitoria()
 }
 
 
-const pegarValor = (event) => {
+const pegarValorTeclado = (event) => {
     const letra = event.target.innerHTML
     const tecla = event.target
+
     compararLetras(letra, tecla)
 }
 
@@ -80,7 +111,9 @@ const verificarDerrota = () => {
     const incorretas = document.getElementsByClassName('incorreta').length
 
     if(incorretas > 6){
+
         alert("Você perdeu")
+
         setTimeout(() => {
              window.location.reload()}, 2000)
     }
@@ -88,13 +121,13 @@ const verificarDerrota = () => {
 }
 const verificarVitoria = () => {
     const inativos = document.getElementsByClassName('inativo')
+
     if(inativos.length == 0){
+
         alert("Você venceu!!!!")
+
         setTimeout(() => {
             window.location.reload()}, 2000)
     }
 }
-atribuirValores()  
-
-atribuirTracos()
-
+atribuirValoresDoTeclado() 
